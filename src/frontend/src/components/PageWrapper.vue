232e45661f771aa9926152b758889defc03c2480
Jakob K <dev@jakobk.de>
2024-01-22 23:33:51 +0100

    Setup structure for protocol creation page
    This also introduces some first design elements using
    tabler.io.

--- src/frontend/src/components/PageWrapper.vue
+++ src/frontend/src/components/PageWrapper.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="content-wrapper">
-    <slot/>
+  <div class="flex-center">
+    <div class="content-wrapper">
+      <slot/>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.flex-center {
+  display: flex;
+  justify-content: center;
+}
+
 .content-wrapper {
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 512px;
+  padding-left: 1em;
+  padding-right: 1em;
+  padding-top: 1em;
+
+  min-width: 512px;
+
+  @media (max-width: 512px) {
+    min-width: 100%;
+  }
+}
+</style>
+
+<style>
+:root {
+  font-size: 1.1em;
+  --tblr-font-sans-serif: 'Inter var', 'Segoe UI', sans-serif;
 }
 </style>
--- src/frontend/src/pages/CreateProtocolPage.vue
+++ src/frontend/src/pages/CreateProtocolPage.vue
@@ -1,5 +1,97 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+function getDateToday() {
+  const date = new Date();
+  return date.toISOString().split('T')[0];
+}
+
+function getDateYesterday() {
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  return date.toISOString().split('T')[0];
+}
+
+const dateInput = ref(getDateToday())
+
+function showDatePicker() {
+  (document.getElementById("datepicker") as HTMLInputElement).showPicker()
+}
+</script>
+
 <template>
-  <button>
-    Submit
-  </button>
+  <h2>Protokoll beginnen</h2>
+  <form class="column" style="--tblr-body-bg: #F7F8FA;">
+    <!-- Zeitdaten -->
+    <fieldset class="form-fieldset">
+      <label class="form-label">Datum</label>
+      <div class="column" style="gap: 0.5em; margin-bottom: 1em;">
+        <div style="display: flex; gap: 0.4em; justify-content: center;">
+          <input type="date" v-model="dateInput" class="form-control" id="datepicker">
+          <button
+              class="btn btn-secondary column"
+              @click="showDatePicker"
+              style="font-size: 0.7em; padding-top: 0.05em; padding-bottom: 0.05em; justify-self: end;"
+            >
+            <div>Datum</div>
+            <div>Wählen</div>
+          </button>
+        </div>
+        <div style="display: flex; gap: 0.4em; align-content: stretch;">
+          <button
+            @click="dateInput = getDateToday()"
+            class="btn" :class="{ 'btn-azure': dateInput == getDateToday() }"
+            style="flex-grow: 1;"
+          >Heute</button>
+          <button
+            @click="dateInput = getDateYesterday()"
+            class="btn" :class="{ 'btn-azure': dateInput == getDateYesterday() }"
+            style="flex-grow: 1;"
+          >Gestern</button>
+        </div>
+      </div>
+      <label class="form-label">Zeitraum</label>
+      <div style="display: flex; gap: 0.5em; align-items: end;">
+        <div class="column" style="align-items: center; gap: 0.1em">
+          <input type="time" class="form-control" style="min-width: 8em; text-align: center;">
+          <div style="font-size: 0.8em;">von</div>
+        </div>
+        <div class="column" style="align-items: center; gap: 0.1em">
+          <div class="dropdown">
+            <div class="btn dropdown-toggle" data-bs-toggle="dropdown">manuell abmelden</div>
+            <div class="dropdown-menu">
+              <div class="dropdown-item">manuell abmelden</div>
+              <div class="dropdown-item">in 1h</div>
+              <div class="dropdown-item">in 2h</div>
+              <div class="dropdown-item">in 3h</div>
+              <div class="dropdown-item">in 4h</div>
+            </div>
+          </div>
+          <div style="font-size: 0.8em;">bis</div>
+        </div>
+      </div>
+    </fieldset>
+
+    <!-- Unterschreiben -->
+
+
+    <!-- Flugzeugdaten -->
+    
+
+    <!-- Abschicken / Beenden -->
+    <fieldset class="form-fieldset">
+      <label class="form-label">Abschließen</label>
+      <div style="display: flex; gap: 0.5em;">
+        <button class="btn btn-primary">Protokoll erstellen</button>
+        <a href="#" class="btn btn-outline-danger">Abbrechen</a>
+      </div>
+    </fieldset>
+  </form>
 </template>
+
+<style scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+}
+</style>
