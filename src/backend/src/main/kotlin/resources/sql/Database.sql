61ff0455af41e453ff13eaf38514af58571f34fb
Laurin Wobeda <s85641@stud.htw-dresden.de>
2024-04-15 17:51:34 +0200

    alligned create statements with changed schema in #76

--- src/backend/src/main/kotlin/resources/sql/Database.sql
+++ src/backend/src/main/kotlin/resources/sql/Database.sql
@@ -1,32 +1,32 @@
 CREATE DATABASE Modellflugplatz;
 
 CREATE TABLE Nutzer(
-	NutzerID VARCHAR(3) not null, CONSTRAINT PK_NutzerID PRIMARY KEY CLUSTERED (NutzerID),
+	NutzerID VARCHAR(3) not null, CONSTRAINT PK_NutzerID PRIMARY KEY (NutzerID),
 	Name VARCHAR(100) not null,
+	Registriernummer VARCHAR(16) not null,
+	hashedPasswort VARCHAR(256),
 	Adresse VARCHAR(100),
-	Adminrechte BOOLEAN not null)
+	Adminrechte BOOLEAN not null
+)
 
 CREATE TABLE Modell(
-	ModellID VARCHAR(3) not null, CONSTRAINT PK_ModellID PRIMARY KEY CLUSTERED (ModellID),
+	ModellID VARCHAR(3) not null, CONSTRAINT PK_ModellID PRIMARY KEY (ModellID),
 	Bezeichnung VARCHAR(100),
 	Klasse VARCHAR (20) not Null,
-	Besitzer VARCHAR (3) not null, CONSTRAINT FK_Besitzer FOREIGN KEY (Besitzer) REFERENCES NutzerID(Nutzer))
+	Besitzer VARCHAR (3) not null, CONSTRAINT FK_Besitzer FOREIGN KEY (Besitzer) REFERENCES NutzerID(Nutzer)
+)
 
 CREATE TABLE Protokoll(
-	ProtokollID VARCHAR(3) not null, CONSTRAINT PK_ProtokollID PRIMARY KEY CLUSTERED (ProtokollID),
+	ProtokollID VARCHAR(3) not null, CONSTRAINT PK_ProtokollID PRIMARY KEY (ProtokollID),
 	ErstellerID VARCHAR(3) not null, CONSTRAINT FK_Ersteller FOREIGN KEY (ErstellerID) REFERENCES NutzerID(Nutzer),
 	Flugbeginn TIME,
 	Flugende TIME,
 	Unterschrift VARBINARY(),
-	Erste_Hilfe VARCHAR(20),
-	Bermekungen VARCHAR(250))
-
-CREATE TABLE Flug(
-	FlugID VARCHAR(3) not null, CONSTRAINT PK_FlugID PRIMARY KEY CLUSTERED (FlugID),
-	ProtokollID VARCHAR(3) not null, CONSTRAINT FK_ProtokollID FOREIGN KEY (ProtokollID) REFERENCES ProtokollID(Protokoll),
-	TeilnehmerID VARCHAR(3) not null,
+	ErsteHilfe VARCHAR(20),
+	Bemerkungen VARCHAR(250),
 	Flugleiter BOOLEAN not null,
-	Modell VARCHAR(3), CONSTRAINT FK_Modell FOREIGN KEY (Modell) REFERENCES (ModellID))
+	Modell VARCHAR(3), CONSTRAINT FK_Modell FOREIGN KEY (Modell) REFERENCES ModellID(Modell)
+)
 
 
 
