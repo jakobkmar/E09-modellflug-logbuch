b6024929519aa3c558d337dae23d41a76b19d3bb
Conrad <conradcaffier2@stud.htw-dresden.de>
2024-04-23 16:48:10 +0200

    new version of the login page

--- src/frontend/src/pages/Login.vue
+++ src/frontend/src/pages/Login.vue
@@ -1,40 +1,68 @@
 <template>
-<div class="card group-card">
-    <div style="display: flex; flex-direction: column; gap: 2em">
-        <div class="card group-card">
-            <h2 style="padding-top: 1.5em; padding-left: 1.4em; padding-bottom: 1.5em;"> Melde dich mit deinem Pilot Account an </h2>
-        </div>
-        <div class="card group-card" style="align-self: center;">
-            <div class="1h-1" style="align-self: center;">
-                <div style="padding-left: 1.5em; padding-right: 1.5em; padding-top: 0.5em; padding-bottom: 0.5em;">Bitte gib hier deine Email ein.</div>
-                <div style="padding-left: 1.5em; padding-bottom: 1.5em;">
-                    <input style="padding-top: 1em; padding-bottom: 1em;" class="btn btn-white" type="text" v-model="email" placeholder="Enter your Email" />
+    <h2>Login</h2>
+    <form class="column" style="--tblr-body-bg: #F7F8FA">
+        <fieldset class="form-fieldset">
+            <label class="form-label">Melde dich mit deinem Account an</label>
+            <div class="column" style="gap: 0.5em; margin-bottom: 1em;">
+                <div style="display: flex; gap: 0.4em; justify-content: center;">
+                        <input style="scoped" class="form-control" text-align="center" type="text" v-model="email" placeholder="Gib deine Email an">
                 </div>
             </div>
-        </div>
-        <div class="card group-card" style="align-self: center;">
-            <div class="1h-1" style="align-self: center;">
-                <div style="padding-left: 1em; padding-right: 1em; padding-top: 0.5em; padding-bottom: 0.5em;">Bitte gib hier dein Passwort ein.</div>
-                <div style="padding-left: 1.5em; padding-bottom: 1em;">
-                    <input style="padding-top: 1em; padding-bottom: 1em;" class="btn btn-white" type="text" v-model="password" placeholder="Enter Password" />
+            <div class="column" style="gap 0.5em; margin-bottom: 1em;">
+                <div style="display: flex; gap: 0.4em; justify-content: center;">
+                        <input
+                        class="form-control"
+                        :type="showPassword ? 'text' : 'password'"
+                        id="password"
+                        name="password"
+                        v-model="password"
+                        required
+                        placeholder="Gib dein Password an"
+                        />
+                        <button type="button" class="btn btn-secondary" @click="togglePassword">
+                            {{ showPassword ? 'Zeigen' : 'Verstecken' }}
+                        </button>
                 </div>
             </div>
-        </div>
-        <div style="padding-bottom: 1em; padding-left: 7em;">
-            <button  class="1h-1" style="padding-left: 6.9em; padding-right: 5em;padding-bottom: 3.5; padding-top: 0em;">
-                    <div style="padding-top: 1.5em; padding-right: 1.8em; padding-bottom: 1.5em;">
-                        <h3>Login</h3>
-                    </div>
-            </button>
-        </div>
-    </div>
-</div>
-
-
+            <div style="display: flex; gap: 0.5em;">
+                <button class="btn btn-primary"> Einloggen </button>
+                <a href="#" class="btn btn-outline-danger">Abbrechen</a>
+            </div>
+        </fieldset>
+    </form>
 </template>
 
-<script>
-export default{
-    name:'Login'
-}
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const email = ref('');
+    const password = ref('');
+    const showPassword = ref(false);
+
+    const togglePassword = () => {
+      showPassword.value = !showPassword.value;
+    };
+
+    const login = () => {
+      // Handle login logic here
+    };
+
+    return {
+      email,
+      password,
+      showPassword,
+      togglePassword,
+      login,
+    };
+  },
+});
 </script>
+
+<style scoped>
+.password-input {
+  display: flex;
+  align-items: center;
+}
+</style>
