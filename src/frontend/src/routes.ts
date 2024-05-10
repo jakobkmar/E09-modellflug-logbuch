a2649589096571806ec69db61560b9409da3e0f4
Conrad <conradcaffier2@stud.htw-dresden.de>
2024-05-10 11:55:40 +0200

    Route for Registration Page

--- src/frontend/src/routes.ts
+++ src/frontend/src/routes.ts
@@ -3,10 +3,13 @@ import CreateProtocolPage from './pages/CreateProtocolPage.vue'
 import ListProtocolsPage from './pages/ListProtocolsPage.vue'
 import CompleteProtocolPage from './pages/CompleteProtocolPage.vue'
 import Login from './pages/Login.vue'
+import RegistrationPage from './pages/RegistrationPage.vue'
+
 
 export const routes = [
   { path: '/', component: HomePage },
   { path: '/Login', component: Login},
+  { path: '/Registration', component: RegistrationPage},
   {
     path: '/protocol',
     children: [
